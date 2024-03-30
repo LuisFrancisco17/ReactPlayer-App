6620b6fc7194629ddae882f2af0bea9359ae5f59
Luis Gil <luisfrancisco17msn@gmail.com>
2024-03-30 10:45:25 -0500

    Modificando propiedades de video

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from "react";
+import ReactPlayer from "react-player";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [playing, setPlaying] = useState(false);
 
-export default App
+  const handlePlay = () => {
+    setPlaying(!playing);
+  };
+
+  return (
+    <div style={{ width: "800px", margin: "auto" }}>
+      <button onClick={handlePlay}>Parar/Reproducir</button>
+      <br />
+      <br />
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+        controls
+        muted
+        loop
+        playing={playing}
+        width={800}
+        height={500}
+      />
+    </div>
+  );
+};
+
+export default App;
